43689af64adcb4e7fbb96227b3d0117d8bb1c86d
jazzrebelbandoleros <lizatimoshenko123@gmail.com>
2024-11-22 17:03:04 +0300

    change all

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,125 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import axios from "axios";
+import { useEffect } from "react";
+import "./App.css";
+import ImageGallery from "./components/ImageGallery/ImageGallery";
+import { fetchPhotos } from "./services/api";
+import SearchBar from "./components/SearchBar/SearchBar";
+import toast, { Toaster } from "react-hot-toast";
+import ImageModal from "./components/ImageModal/ImageModal";
+import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn";
+import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
+import Loader from "./components/Loader/Loader";
+import { ModalProps, Photo } from "./types";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export interface ApiResponse {
+  results: Photo[];
+  total_pages: number;
 }
 
-export default App
+function App() {
+  const [photos, setPhotos] = useState<Photo[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<boolean>(false);
+  const [page, setPage] = useState<number>(1);
+  const [topic, setTopic] = useState<string>("");
+  const [totalPages, setTotalPages] = useState<number>(0);
+  const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
+  const [selectedImageUrl, setSelectedImageUrl] = useState<ModalProps | null>(
+    null
+  );
+
+  const handleSearch = async (newTopic: string) => {
+    setPhotos([]);
+    setPage(1);
+    setTopic(newTopic);
+  };
+
+  const handleLoadMore = () => {
+    console.log(handleLoadMore);
+    setPage(page + 1);
+  };
+
+  const handleImageClick = (imageData: ModalProps) => {
+    setSelectedImageUrl(imageData);
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
+  useEffect(() => {
+    if (topic === "") {
+      return;
+    }
+    async function getPhotos() {
+      try {
+        setLoading(true);
+        setError(false);
+        const { results, total_pages }: ApiResponse = await fetchPhotos(
+          topic,
+          page
+        );
+        if (page === 1) {
+          setTotalPages(total_pages);
+        }
+        setPhotos((prevPhotos) => {
+          return [...prevPhotos, ...results];
+        });
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    }
+    getPhotos();
+  }, [page, topic]);
+
+  return (
+    <div>
+      <SearchBar onSubmit={handleSearch} />
+      {photos.length > 0 && (
+        <ImageGallery items={photos} onImageClick={handleImageClick} />
+      )}
+      {loading && <Loader isLoading={loading} />}
+      {error && (
+        <ErrorMessage
+          message={
+            "Ops, something went wrong :( please try reloading this page!"
+          }
+        />
+      )}
+      {photos.length > 0 && !loading && page < totalPages && (
+        <LoadMoreBtn onClick={handleLoadMore} />
+      )}
+      <div>
+        <Toaster
+          toastOptions={{
+            className: "",
+            style: {
+              border: "1px solid #006400",
+              padding: "16px",
+              color: "#006400",
+              background: "#228B22",
+            },
+          }}
+          containerStyle={{
+            position: "relative",
+          }}
+        />
+        {selectedImageUrl && (
+          <ImageModal
+            isOpen={modalIsOpen}
+            onRequestClose={closeModal}
+            imageUrl={selectedImageUrl.imageUrl}
+            author={selectedImageUrl.author}
+            description={selectedImageUrl.description}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
